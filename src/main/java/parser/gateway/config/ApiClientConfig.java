f154f093c80ffe673cb079b00eeadcf1c838f54c
Therenegadee <melkindaniil@yandex.ru>
2023-10-29 21:59:49 +0300

    fixed errors

--- src/main/java/parser/gateway/config/ApiClientConfig.java
+++ src/main/java/parser/gateway/config/ApiClientConfig.java
@@ -4,12 +4,11 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import parser.gateway.openapi.ApiClient;
 import parser.gateway.openapi.api.AuthorizationApi;
 import parser.gateway.openapi.api.UserApi;
 import parser.gateway.utils.TokenKeeper;
-
-import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,29 +17,28 @@ public class ApiClientConfig {
     @Autowired
     private final TokenKeeper tokenKeeper;
 
-    @Bean
-    public ApiClient userClient() {
-        ApiClient apiClient = parser.gateway.openapi.Configuration.getDefaultApiClient();
 
-        apiClient.setRequestInterceptor((http) -> {
-            http.setHeader(
-                    "Authorization",
-                    "Bearer "
-                            + tokenKeeper.getToken("API_ADMIN", List.of("ROLE_ADMIN"))
-            );
-        });
-
-        return apiClient;
-    }
-
-    @Bean
-    public UserApi userApi(ApiClient apiClient) {
-        UserApi userApi = new UserApi(apiClient);
-        return userApi;
-    }
-
-    @Bean
-    public AuthorizationApi authorizationApi(ApiClient apiClient){
-        return new AuthorizationApi(apiClient);
-    }
+//    @Bean
+//    public ApiClient userClient(String jwtToken) {
+//        ApiClient apiClient = parser.gateway.openapi.Configuration.getDefaultApiClient();
+//
+//        apiClient.setRequestInterceptor((http) -> {
+//            http.setHeader(
+//                    "Authorization",
+//                    "Bearer " + jwtToken);
+//        });
+//
+//        return apiClient;
+//    }
+//
+//    @Bean
+//    public UserApi userApi(ApiClient apiClient) {
+//        UserApi userApi = new UserApi(apiClient);
+//        return userApi;
+//    }
+//
+//    @Bean
+//    public AuthorizationApi authorizationApi(ApiClient apiClient){
+//        return new AuthorizationApi(apiClient);
+//    }
 }
--- src/main/java/parser/gateway/filter/AuthFilter.java
+++ src/main/java/parser/gateway/filter/AuthFilter.java
@@ -8,8 +8,9 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.server.ServerWebExchange;
+import parser.gateway.config.ApiClientConfig;
+import parser.gateway.openapi.ApiClient;
 import parser.gateway.openapi.ApiException;
 import parser.gateway.openapi.api.UserApi;
 import parser.gateway.openapi.model.JwtResponseOpenApi;
@@ -22,8 +23,6 @@ import java.util.Objects;
 
 @Component
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
-    @Autowired
-    private UserApi userApi;
     @Autowired
     private PathUtils pathUtils;
     @Autowired
@@ -39,40 +38,70 @@ public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> 
             if (!exchange.getRequest().getHeaders().containsKey(HttpHeaders.AUTHORIZATION)) {
                 onError(exchange, "Missing authorization header");
             }
-
-            String authHeader = exchange
-                    .getRequest()
-                    .getHeaders()
-                    .get(HttpHeaders.AUTHORIZATION)
-                    .get(0);
-
-            String[] authHeaderParts = authHeader.split(" ");
-            if (authHeaderParts.length != 2 || !"Bearer".equals(authHeaderParts[0])) {
-                onError(exchange, "Incorrect auth header structure");
-            }
-
-            JwtResponseOpenApi jwtResponse =  null;
-            try {
-                jwtResponse = userApi.validateToken(authHeaderParts[1]);
-            } catch (ApiException e) {
-                onError(exchange, e.getMessage());
-            }
-            if(Objects.isNull(jwtResponse)) {
-                onError(exchange, "Jwt Response is Null");
-            }
+            String jwtToken = getJwtToken(exchange);
+            JwtResponseOpenApi jwtResponse = sendValidationTokenRequest(exchange, jwtToken);
             List<String> roles = jwtResponse.getRoles();
-            String requestPath = exchange.getRequest().getURI().getPath();
-            if(!pathUtils.getPathsByRoles().containsKey(requestPath)) {
-                throw new BadRequestException(String.format("The request uri %s doesn't exists in allowed list!", requestPath));
-            }
-
-            List<String> allowedRoles = pathUtils.getPathsByRoles().get(requestPath);
-            boolean accessAllowed = allowedRoles.stream().anyMatch(roles::contains);
-            if(!accessAllowed) {
-               onError(exchange, "Access isn't Allowed! Don't have necessary rights");
-            }
+            checkAccessAllowed(exchange, roles);
             return chain.filter(exchange);
         });
+    }
+
+    private String getJwtToken(ServerWebExchange exchange) {
+        String authHeader = exchange
+                .getRequest()
+                .getHeaders()
+                .get(HttpHeaders.AUTHORIZATION)
+                .get(0);
+        String[] authHeaderParts = authHeader.split(" ");
+        if (authHeaderParts.length != 2 || !"Bearer".equals(authHeaderParts[0])) {
+            onError(exchange, "Incorrect auth header structure");
+        }
+        String jwtToken = authHeaderParts[1];
+        return jwtToken;
+    }
+
+    private JwtResponseOpenApi sendValidationTokenRequest(ServerWebExchange exchange, String jwtToken) {
+        ApiClient apiClient = createApiClient(jwtToken);
+        UserApi userApi = createUserApiClient(apiClient);
+        JwtResponseOpenApi jwtResponse;
+        try {
+            userApi.validateTokenWithHttpInfo(jwtToken);
+            jwtResponse = userApi.validateToken(jwtToken);
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
+        if (Objects.isNull(jwtResponse)) {
+            onError(exchange, "Jwt Response is Null");
+        }
+        return jwtResponse;
+    }
+
+    private ApiClient createApiClient(String jwtToken) {
+        ApiClient apiClient = parser.gateway.openapi.Configuration.getDefaultApiClient();
+        apiClient.setRequestInterceptor((http) -> {
+            http.setHeader(
+                    "Authorization",
+                    "Bearer " + jwtToken);
+        });
+        return apiClient;
+    }
+
+    private UserApi createUserApiClient(ApiClient apiClient) {
+        UserApi userApi = new UserApi(apiClient);
+        return userApi;
+    }
+
+    private void checkAccessAllowed(ServerWebExchange exchange, List<String> roles) {
+        String requestPath = exchange.getRequest().getURI().getPath();
+        if (!pathUtils.getPathsByRoles().containsKey(requestPath)) {
+            throw new BadRequestException(String.format("The request uri %s doesn't exists in allowed list!", requestPath));
+        }
+
+        List<String> allowedRoles = pathUtils.getPathsByRoles().get(requestPath);
+        boolean accessAllowed = allowedRoles.stream().anyMatch(roles::contains);
+        if (!accessAllowed) {
+            onError(exchange, "Access isn't Allowed! Don't have necessary rights");
+        }
     }
 
     private Mono<Void> onError(ServerWebExchange exchange, String message) {
